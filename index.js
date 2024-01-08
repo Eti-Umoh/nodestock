38c92416d176cfe374b4ee21b77e00e41e39dd3e
Eti-Umoh <etiumoh04@gmail.com>
2024-01-08 20:24:13 +0100

    Major changes

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
-
+const request = require('request')
 const app = express();
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 //pk_249c4987c73a4486ae06e9a8298c0f5a API KEY
+function callApi(finishedApi) {
+    request('https://api.iex.cloud/v1/data/core/quote/aapl?token=pk_249c4987c73a4486ae06e9a8298c0f5a', {json: true}, (err, res, body) => {
+    if (err) {return console.log(err);}
+    if (res.statusCode == 200) {
+        finishedApi(body);
+    };
+});
+}
+
 
 //Set handlebars middleware
 app.engine('handlebars', engine());
@@ -16,16 +25,20 @@ app.set('views', './views');
 const otherStuff = "Just some other stuff"
 //Set handlebar rotes
 app.get('/', (req, res) => {
-    res.render('home', {
-        stuff: otherStuff
+    callApi(function (doneApi){
+        res.render('home', {
+            stock: doneApi
+        });
     });
 });
+
 
 app.get('/about.html', (req, res) => {
     res.render('about', {
         stuff: otherStuff
     });
 });
+
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
