29fcb6943e64a5f2320917dba0b1cca1d7e9796f
Eti-Umoh <etiumoh04@gmail.com>
2024-01-08 18:08:48 +0100

    set up about.handlebars

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
+//pk_249c4987c73a4486ae06e9a8298c0f5a API KEY
+
 //Set handlebars middleware
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -15,6 +17,12 @@ const otherStuff = "Just some other stuff"
 //Set handlebar rotes
 app.get('/', (req, res) => {
     res.render('home', {
+        stuff: otherStuff
+    });
+});
+
+app.get('/about.html', (req, res) => {
+    res.render('about', {
         stuff: otherStuff
     });
 });
